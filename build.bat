4b014f58f00e80f4c668e833474c45520a5317ba
zsnvwpsvwsvi-a11y <zsnvwpsvwsvi@gmail.com>
2025-08-14 18:49:35 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-echo Compiling SDL project...
-
-g++ main.cpp editor_core.cpp -o main.exe -mconsole -IC:\msys64\mingw64\include\SDL2 -LC:\msys64\mingw64\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_TTF -lcomdlg32
-main.exe
-pause
-
